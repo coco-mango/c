ddedb01479d3a08272054aff02bfb96785cd63ea
coco-mango <154478990+coco-mango@users.noreply.github.com>
2023-12-31 13:14:12 +0900

    Create PT4_sum.cpp

---
+++ c++/명품C++Programmer/Chap1/PT4_sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int sum;
+	sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+	cout << "1에서 10까지 더한 결과는" << sum << "입니다\n";
+	return 0;
+}
+
+//함수를 사용하지 않고 단순히 1~10까지 더한 값을 sum변수에 반환하여 출력
