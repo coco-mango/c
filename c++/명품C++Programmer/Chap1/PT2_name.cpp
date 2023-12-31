7032d44836d1851a01f267c793942a311e97b0a0
coco-mango <154478990+coco-mango@users.noreply.github.com>
2023-12-31 13:12:59 +0900

    Update PT2_name.cpp

--- c++/명품C++Programmer/Chap1/PT2_name.cpp
+++ c++/명품C++Programmer/Chap1/PT2_name.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main() {
 	cout << "My name is coconut\n";         // '\n' 다음라인으로 넘어가기
-	return 0;
+	return 0;                               // return 0 : main 함수가 종료되면 프로그램도 함께 종료
+	                                        // main함수가 운영체제에게 작업을 완료했다고 알림
 }
