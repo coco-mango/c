9f0ac68dd9089d5d8a573a678717abad4545979e
coco-mango <154478990+coco-mango@users.noreply.github.com>
2023-12-31 13:07:26 +0900

    Create PT1_add.cpp

---
+++ c++/명품C++Programmer/Chap1/PT1_add.cpp
@@ -0,0 +1,11 @@
+#include <iostream>   //input,output 하기 위해 iostream 추가
+using namespace std;  //std:: 생략하기 위해 'using namespace std;' 추가
+
+int main() {
+	int a = 10;
+	int b = 20;
+	int sum;
+
+	sum = a + b;
+	cout << sum;        // 출력 : cout
+}
